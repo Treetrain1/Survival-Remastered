1753490816522ef0d3c7499e320fd2c1bdd8155c
Treetrain1 <treetrain1@gmail.com>
2022-08-07 21:38:16 -0500

    prepare for beta 4

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -9,10 +9,10 @@ console.info('Hello, World! (You will see this line every time server resources 
 
 onEvent('recipes', event => {
 	// Change recipes here
+	event.remove({output: 'techreborn:block_breaker'})
 })
 
 onEvent('item.tags', event => {
-	event.remove({output: 'techreborn:block_breaker'})
 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
 	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
 
