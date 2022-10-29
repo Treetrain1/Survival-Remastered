43c6236e53614b424388618f9c4b93d6e6872101
Treetrain1 <treetrain1@gmail.com>
2022-10-29 14:51:28 -0500

    update

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -1,8 +1,8 @@
 // priority: 0
 
-console.info('Hello, World! (You will see this line every time client resources reload)')
+//console.info('Hello, World! (You will see this line every time client resources reload)')
 
-onEvent('jei.hide.items', event => {
+/*onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
-})
+})*/
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,18 +1,18 @@
 // priority: 0
 
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
+//settings.logAddedRecipes = true
+//settings.logRemovedRecipes = true
+//settings.logSkippedRecipes = false
+//settings.logErroringRecipes = true
 
-console.info('Hello, World! (You will see this line every time server resources reload)')
+//console.info('Hello, World! (You will see this line every time server resources reload)')
 
-onEvent('recipes', event => {
+ServerEvents.recipes(event => {
 	// Change recipes here
-	event.remove({output: 'techreborn:block_breaker'})
+	//event.remove({output: 'techreborn:block_breaker'})
 })
 
-onEvent('item.tags', event => {
+ServerEvents.tags('item', event => {
 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
 	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
 
